ad22948f32d3b9f8ebe21206b5f1114b37f63e5b
shepherrrd <shefkid2@gmail.com>
2025-07-12 00:38:25 +0100

    Cask Update

--- Casks/cliped-crossplatform.rb
+++ Casks/cliped-crossplatform.rb
@@ -1,26 +1,23 @@
-class ClipedCrossplatform < Formula
+cask "cliped-crossplatform" do
+  version "1.0.0"
+  sha256 "f5e49ceda921df88665056a4787bd33a37b331aa6e870dd32fe2d109020e0fc5"
+
+  url "https://github.com/shepherrrd/cliped-crossplatform/releases/download/v#{version}/cliped-macos.tar.gz"
+  name "Cliped Cross-Platform"
   desc "Beautiful cross-platform clipboard manager"
   homepage "https://github.com/shepherrrd/cliped-crossplatform"
-  url "https://github.com/shepherrrd/cliped-crossplatform/releases/download/v1.0.0/cliped-macos.tar.gz"
-  sha256 "f5e49ceda921df88665056a4787bd33a37b331aa6e870dd32fe2d109020e0fc5"
-  version "1.0.0"
 
-  def install
-    prefix.install "Cliped.app"
-    bin.write_exec_script "#{prefix}/Cliped.app/Contents/MacOS/Cliped"
+  livecheck do
+    url :url
+    strategy :github_latest
   end
 
-  def caveats
-    <<~EOS
-      To start Cliped:
-        cliped
+  app "Cliped.app"
 
-      Or run directly:
-        open #{prefix}/Cliped.app
-    EOS
-  end
-
-  test do
-    assert_predicate prefix/"Cliped.app", :exist?
-  end
+  zap trash: [
+    "~/Library/Application Support/com.cliped.app",
+    "~/Library/Caches/com.cliped.app",
+    "~/Library/Preferences/com.cliped.app.plist",
+    "~/Library/Saved Application State/com.cliped.app.savedState",
+  ]
 end
